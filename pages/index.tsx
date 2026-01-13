e478df7e29effb6a7cc50c5ccb5930d5a043e702
Zhenua82 <zzhmenka@yandex.ru>
2026-01-13 12:09:10 +0300

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const HomePage: React.FC<Props> = ({ plants }) => {
         />
       </Head>
       <section className={styles.hero}>
-        <h1>Питомник хвойных растений</h1>
+        <h1>Питомник хвойных растений</h1><span style={{color: 'red'}}> Pages Router (button in component)</span>
         <p>Короткое приветствие — лучшие саженцы ели, сосны и можжевельника.</p>
       </section>
 
